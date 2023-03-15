93a62580e6be5cca9cb27f7d7c91e67efbf48e6e
pangaribuanantonius <pangaribuanantonius@gmail.com>
2023-03-15 11:55:32 +0700

    aku dah berhasil buat profil menu tentang

--- app/Http/Controllers/TentangController.php
+++ app/Http/Controllers/TentangController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use App\Models\Tentang;
 use Illuminate\Http\Request;
 
-class SejarahController extends Controller
+class TentangController extends Controller
 {
-    public function(tentang){
+    public function tentang(){
         $tentang = \App\Models\Tentang::all();
-        return view('tentang', ['tentang' => $tentang);
+        return view('tentang', ['tentang' => $tentang]);
     }
 }
