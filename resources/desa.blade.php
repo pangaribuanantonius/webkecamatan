377e0095b3c394d6baeba93c77830f7381db2498
pangaribuanantonius <pangaribuanantonius@gmail.com>
2023-03-17 11:16:32 +0700

    aku menambahkan desa php

---
+++ resources/desa.blade.php
@@ -0,0 +1,44 @@
+<br><br><br><br><br>
+@extends('main')
+@section('layout')
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="card col-8" align="justify">
+            <p>
+              <h2><strong>Kepegawaian</strong></h2>
+              <br>
+              <table class="table table-bordered" bordered="1" width="100%" cellspacing="0" cellpadding="1">
+                <thead>
+                  <tr class="text-center">
+                    <th>NIP</th>
+                    <th>NAMA</th>
+                    <th>GOLONGAN</th>
+                    <th>JABATAN</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach($kepegawaian as $pegawai)
+                  <tr>
+                    <td>{{$pegawai->nip}}</td>
+                    <td>{{$pegawai->nama}}</td>
+                    <td>{{$pegawai->golongan}}</td>
+                    <td>{{$pegawai->jabatan}}</td>
+                  </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </p>
+          </div>
+          <div class="col-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    </section><br><br>
+@endsection
