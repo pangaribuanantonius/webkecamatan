f3da5f1c8b4cd42780908787b52aed8f08343409
pangaribuanantonius <pangaribuanantonius@gmail.com>
2023-05-16 13:49:13 +0700

    satpam login di routesnya

--- routes/web.php
+++ routes/web.php
@@ -75,58 +75,58 @@ Route::get('/videoall', [VideoAllController::class, 'videoall'])->name('videoall
 
 //admin
 Route::get('/info/index', [InfoController::class, 'index'])->name('info.index')->middleware('login');
-Route::get('/info/create', [InfoController::class, 'create'])->name('info.create');
-Route::post('/info/store', [InfoController::class, 'store'])->name('info.store');
-Route::get('/info/{info}/edit', [InfoController::class, 'edit'])->name('info.edit');
-Route::patch('/info/{info}/edit', [InfoController::class, 'update'])->name('info.edit');
-Route::delete('/info/{info}/delete', [InfoController::class, 'destroy'])->name('info.delete');
+Route::get('/info/create', [InfoController::class, 'create'])->name('info.create')->middleware('login');
+Route::post('/info/store', [InfoController::class, 'store'])->name('info.store')->middleware('login');
+Route::get('/info/{info}/edit', [InfoController::class, 'edit'])->name('info.edit')->middleware('login');
+Route::patch('/info/{info}/edit', [InfoController::class, 'update'])->name('info.edit')->middleware('login');
+Route::delete('/info/{info}/delete', [InfoController::class, 'destroy'])->name('info.delete')->middleware('login');
 
-Route::get('/profil/index', [ProfilController::class, 'index'])->name('profil.index');
-Route::get('/profil/create', [ProfilController::class, 'create'])->name('profil.create');
-Route::post('/profil/store', [ProfilController::class, 'store'])->name('profil.store');
-Route::get('/profil/{profil}/edit', [ProfilController::class, 'edit'])->name('profil.edit');
-Route::patch('/profil/{profil}/edit', [ProfilController::class, 'update'])->name('profil.edit');
-Route::delete('/profil/{profil}/delete', [ProfilController::class, 'destroy'])->name('profil.delete');
+Route::get('/profil/index', [ProfilController::class, 'index'])->name('profil.index')->middleware('login');
+Route::get('/profil/create', [ProfilController::class, 'create'])->name('profil.create')->middleware('login');
+Route::post('/profil/store', [ProfilController::class, 'store'])->name('profil.store')->middleware('login');
+Route::get('/profil/{profil}/edit', [ProfilController::class, 'edit'])->name('profil.edit')->middleware('login');
+Route::patch('/profil/{profil}/edit', [ProfilController::class, 'update'])->name('profil.edit')->middleware('login');
+Route::delete('/profil/{profil}/delete', [ProfilController::class, 'destroy'])->name('profil.delete')->middleware('login');
 
-Route::get('/visimisi/index', [VisiMisiController::class, 'index'])->name('visimisi.index');
-Route::get('/visimisi/{visimisi}/edit', [VisimisiController::class, 'edit'])->name('visimisi.edit');
-Route::patch('/visimisi/{visimisi}/edit', [VisimisiController::class, 'update'])->name('visimisi.edit');
+Route::get('/visimisi/index', [VisiMisiController::class, 'index'])->name('visimisi.index')->middleware('login');
+Route::get('/visimisi/{visimisi}/edit', [VisimisiController::class, 'edit'])->name('visimisi.edit')->middleware('login');
+Route::patch('/visimisi/{visimisi}/edit', [VisimisiController::class, 'update'])->name('visimisi.edit')->middleware('login');
 
-Route::get('/strukturorganisasi/index', [StrukturorganisasiController::class, 'index'])->name('strukturorganisasi.index');
-Route::get('/strukturorganisasi/{strukturorganisasi}/edit', [StrukturorganisasiController::class, 'edit'])->name('strukturorganisasi.edit');
-Route::patch('/strukturorganisasi/{strukturorganisasi}/edit', [StrukturorganisasiController::class, 'update'])->name('strukturorganisasi.edit');
+Route::get('/strukturorganisasi/index', [StrukturorganisasiController::class, 'index'])->name('strukturorganisasi.index')->middleware('login');
+Route::get('/strukturorganisasi/{strukturorganisasi}/edit', [StrukturorganisasiController::class, 'edit'])->name('strukturorganisasi.edit')->middleware('login');
+Route::patch('/strukturorganisasi/{strukturorganisasi}/edit', [StrukturorganisasiController::class, 'update'])->name('strukturorganisasi.edit')->middleware('login');
 
-Route::get('/kepegawaian/index', [KepegawaianController::class, 'index'])->name('kepegawaian.index');
-Route::get('/kepegawaian/create', [KepegawaianController::class, 'create'])->name('kepegawaian.create');
-Route::post('/kepegawaian/store', [KepegawaianController::class, 'store'])->name('kepegawaian.store');
-Route::get('/kepegawaian/{kepegawaian}/edit', [KepegawaianController::class, 'edit'])->name('kepegawaian.edit');
-Route::patch('/kepegawaian/{kepegawaian}/edit', [KepegawaianController::class, 'update'])->name('kepegawaian.edit');
-Route::delete('/kepegawaian/{kepegawaian}/delete', [KepegawaianController::class, 'destroy'])->name('kepegawaian.delete');
+Route::get('/kepegawaian/index', [KepegawaianController::class, 'index'])->name('kepegawaian.index')->middleware('login');
+Route::get('/kepegawaian/create', [KepegawaianController::class, 'create'])->name('kepegawaian.create')->middleware('login');
+Route::post('/kepegawaian/store', [KepegawaianController::class, 'store'])->name('kepegawaian.store')->middleware('login');
+Route::get('/kepegawaian/{kepegawaian}/edit', [KepegawaianController::class, 'edit'])->name('kepegawaian.edit')->middleware('login');
+Route::patch('/kepegawaian/{kepegawaian}/edit', [KepegawaianController::class, 'update'])->name('kepegawaian.edit')->middleware('login');
+Route::delete('/kepegawaian/{kepegawaian}/delete', [KepegawaianController::class, 'destroy'])->name('kepegawaian.delete')->middleware('login');
 
-Route::get('/desa/index', [DesaController::class, 'index'])->name('desa.index');
-Route::get('/desa/create', [DesaController::class, 'create'])->name('desa.create');
-Route::post('/desa/store', [DesaController::class, 'store'])->name('desa.store');
-Route::get('/desa/{desa}/edit', [DesaController::class, 'edit'])->name('desa.edit');
-Route::patch('/desa/{desa}/edit', [DesaController::class, 'update'])->name('desa.edit');
-Route::delete('/desa/{desa}/delete', [DesaController::class, 'destroy'])->name('desa.delete');
+Route::get('/desa/index', [DesaController::class, 'index'])->name('desa.index')->middleware('login');
+Route::get('/desa/create', [DesaController::class, 'create'])->name('desa.create')->middleware('login');
+Route::post('/desa/store', [DesaController::class, 'store'])->name('desa.store')->middleware('login');
+Route::get('/desa/{desa}/edit', [DesaController::class, 'edit'])->name('desa.edit')->middleware('login');
+Route::patch('/desa/{desa}/edit', [DesaController::class, 'update'])->name('desa.edit')->middleware('login');
+Route::delete('/desa/{desa}/delete', [DesaController::class, 'destroy'])->name('desa.delete')->middleware('login');
 
-Route::get('/kegiatan/index', [AdminkegiatanController::class, 'index'])->name('kegiatan.index');
-Route::get('/kegiatan/create', [AdminkegiatanController::class, 'create'])->name('kegiatan.create');
-Route::post('/kegiatan/store', [AdminkegiatanController::class, 'store'])->name('kegiatan.store');
-Route::get('/kegiatan/{kegiatan}/edit', [AdminkegiatanController::class, 'edit'])->name('kegiatan.edit');
-Route::patch('/kegiatan/{kegiatan}/edit', [AdminkegiatanController::class, 'update'])->name('kegiatan.edit');
-Route::delete('/kegiatan/{kegiatan}/delete', [AdminkegiatanController::class, 'destroy'])->name('kegiatan.delete');
+Route::get('/kegiatan/index', [AdminkegiatanController::class, 'index'])->name('kegiatan.index')->middleware('login');
+Route::get('/kegiatan/create', [AdminkegiatanController::class, 'create'])->name('kegiatan.create')->middleware('login');
+Route::post('/kegiatan/store', [AdminkegiatanController::class, 'store'])->name('kegiatan.store')->middleware('login');
+Route::get('/kegiatan/{kegiatan}/edit', [AdminkegiatanController::class, 'edit'])->name('kegiatan.edit')->middleware('login');
+Route::patch('/kegiatan/{kegiatan}/edit', [AdminkegiatanController::class, 'update'])->name('kegiatan.edit')->middleware('login');
+Route::delete('/kegiatan/{kegiatan}/delete', [AdminkegiatanController::class, 'destroy'])->name('kegiatan.delete')->middleware('login');
 
-Route::get('/settings/index', [SettingsController::class, 'index'])->name('settings.index');
-Route::get('/settings/{settings}/edit', [SettingsController::class, 'edit'])->name('settings.edit');
-Route::patch('/settings/{settings}/edit', [SettingsController::class, 'update'])->name('settings.edit');
+Route::get('/settings/index', [SettingsController::class, 'index'])->name('settings.index')->middleware('login');
+Route::get('/settings/{settings}/edit', [SettingsController::class, 'edit'])->name('settings.edit')->middleware('login');
+Route::patch('/settings/{settings}/edit', [SettingsController::class, 'update'])->name('settings.edit')->middleware('login')->middleware('login');
 
-Route::get('/slider/index', [SliderController::class, 'index'])->name('slider.index');
-Route::get('/slider/create', [SliderController::class, 'create'])->name('slider.create');
-Route::post('/slider/store', [SliderController::class, 'store'])->name('slider.store');
-Route::get('/slider/{slider}/edit', [SliderController::class, 'edit'])->name('slider.edit');
-Route::patch('/slider/{slider}/edit', [SliderController::class, 'update'])->name('slider.edit');
-Route::delete('/slider/{slider}/delete', [SliderController::class, 'destroy'])->name('slider.delete');
+Route::get('/slider/index', [SliderController::class, 'index'])->name('slider.index')->middleware('login');
+Route::get('/slider/create', [SliderController::class, 'create'])->name('slider.create')->middleware('login');
+Route::post('/slider/store', [SliderController::class, 'store'])->name('slider.store')->middleware('login');
+Route::get('/slider/{slider}/edit', [SliderController::class, 'edit'])->name('slider.edit')->middleware('login');
+Route::patch('/slider/{slider}/edit', [SliderController::class, 'update'])->name('slider.edit')->middleware('login');
+Route::delete('/slider/{slider}/delete', [SliderController::class, 'destroy'])->name('slider.delete')->middleware('login');
 
 //login
 Route::get('/login/login', [LoginController::class, 'login'])->name('login.login');
@@ -136,9 +136,9 @@ Route::post('/login/login', [LoginController::class, 'postlogin'])->name('login.
 Route::get('/login/logout', [LoginController::class, 'logout'])->name('logout');
 
 //superadmin
-Route::get('/user/index', [UserController::class, 'index'])->name('user.index');
-Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('/user/store', [UserController::class, 'store'])->name('user.store');
-Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
-Route::patch('/user/{user}/edit', [UserController::class, 'update'])->name('user.edit');
-Route::delete('/user/{user}/delete', [UserController::class, 'destroy'])->name('user.delete');
+Route::get('/user/index', [UserController::class, 'index'])->name('user.index')->middleware('login');
+Route::get('/user/create', [UserController::class, 'create'])->name('user.create')->middleware('login');
+Route::post('/user/store', [UserController::class, 'store'])->name('user.store')->middleware('login');
+Route::get('/user/{user}/edit', [UserController::class, 'edit'])->name('user.edit')->middleware('login');
+Route::patch('/user/{user}/edit', [UserController::class, 'update'])->name('user.edit')->middleware('login');
+Route::delete('/user/{user}/delete', [UserController::class, 'destroy'])->name('user.delete')->middleware('login');
